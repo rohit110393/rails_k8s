eb2e94703c4ddf2ef131ef18c4d30f60775293e4
rohit110393 <rohitchauhan.mnnit@gmail.com>
2020-01-29 17:16:13 +0530

    partial commit5

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
-docker build -t roohhiit110393/rails_k8s:latest -t roohhiit110393/rails_k8s:$SHA 
+# docker build -t roohhiit110393/rails_k8s:latest -t roohhiit110393/rails_k8s:$SHA 
 
-docker push roohhiit110393/rails_k8s:latest
+# docker push roohhiit110393/rails_k8s:latest
 
-docker push roohhiit110393/rails_k8s:$SHA
+# docker push roohhiit110393/rails_k8s:$SHA
 
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=roohhiit110393/rails_k8s:$SHA
+kubectl apply -f k8s/k8s_rails-namespace.yaml
+kubectl apply -f k8s/server-deployment.yaml
+kubectl apply -f k8s/server-nodeport.yaml
+kubectl apply -f k8s/k8s_rails-ingress.yaml
+# kubectl set image deployments/server-deployment server=roohhiit110393/rails_k8s:$SHA
