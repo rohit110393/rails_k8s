2a374a51f64edd2a444c7d90c87f4523997ae07b
rohit110393 <rohitchauhan.mnnit@gmail.com>
2020-01-30 12:41:04 +0530

    partial commit20

--- release.sh
+++ release.sh
@@ -4,11 +4,11 @@ echo "start tiller"
 export KUBECONFIG=$HOME/.kube/kubeconfig
 helm tiller start-ci
 export HELM_HOST=127.0.0.1:44134
-result=$(eval helm ls | grep helmCharts) 
+result=$(eval helm ls | grep helloapp) 
 if [ $? -ne "0" ]; then 
-   helm install --timeout 180 --name helmCharts --set image.tag=$TAG charts/helmCharts
+   helm install  --name helloapp --set image.tag=$TAG charts/helloapp
 else 
-   helm upgrade --timeout 180 helmCharts --set image.tag=$TAG charts/helmCharts
+   helm upgrade --name helloapp --set image.tag=$TAG charts/helloapp
 fi
 echo "stop tiller"
 helm tiller stop 
